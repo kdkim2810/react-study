5d246b02a65b0f22862277fe8b8b15e76550fa98
kdkim2810 <kdkim2810@gmail.com>
2020-04-01 19:21:04 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'; // 리액트를 불러오는 구문
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+    const name = '리액트';
+
+    return (
+      <div>
+        {name === '리액트' ? (
+          <h1>리액트입니다.</h1>
+        ) : (
+          <h2>리액트가 아닙니다.</h2>
+        )}
+      </div>
+    );
+}
+
+export default App;
