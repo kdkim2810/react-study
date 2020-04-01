06bff8585f569fd768ac677fedd2316e1797af6b
kdkim2810 <kdkim2810@gmail.com>
2020-04-01 20:11:11 +0900

    undefined를 렌더링 하지 않기 -2

--- src/App.js
+++ src/App.js
@@ -74,8 +74,18 @@ function Example6() {
   return name || '값이 undefined 입니다.';
 }
 
+function Example7() {
+  /*
+  jsx안에서는 undefined를 렌더링 할 수 있는데
+  name값이 undefined일 때 보여주고 싶은 문구가 있다면 다음과 같이 사용 가능
+  */
+  const name = undefined;
+
+  return <div>{name || '리액트'}</div>
+}
+
 function App() {
-    return <Example6/>;
+    return <Example7/>;
 }
 
 export default App;
