13ef51d918eeb6cff2c78dcda0f99ed4233b37e5
kdkim2810 <kdkim2810@gmail.com>
2020-04-01 19:59:53 +0900

    조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -44,8 +44,20 @@ function Example3() {
   )
 }
 
+function Example4() {
+  /*
+  아래처럼 null을 렌더링할수도 있지만 더 짧은 코드로 하려면 &&연산자를 사용하여 조건부 렌더링을 할 수 있다.
+  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  */
+  const name = '리액트';
+
+  return (
+    <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>
+  )
+}
+
 function App() {
-    return <Example3/>;
+    return <Example4/>;
 }
 
 export default App;
