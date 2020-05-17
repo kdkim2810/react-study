dd913d7d5f6f044c2abb060e66688c8eda52541b
kdkim2810 <kdkim2810@gmail.com>
2020-05-17 18:12:24 +0900

    4.2.3 임의 메소드 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; // 리액트를 불러오는 구문
-import Component from './example/chapter_4/MyComponent421';
+import Component from './example/chapter_4/MyComponent4231';
 
 function App() {
   return <Component />;
