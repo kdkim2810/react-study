e7707aba312fa86545fbbfa7660a340b3a5f3876
kdkim2810 <kdkim2810@gmail.com>
2020-05-17 18:27:59 +0900

    4.3 함수형 컴포넌트로 이벤트 핸들링 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; // 리액트를 불러오는 구문
-import Component from './example/chapter_4/MyComponent425';
+import Component from './example/chapter_4/MyComponent43';
 
 function App() {
   return <Component />;
