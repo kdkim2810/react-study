80c2b4f01c413414525e80e0402710f4296600bf
kdkim2810 <kdkim2810@gmail.com>
2020-05-17 17:36:47 +0900

    3.4.2.2 useState 사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'; // 리액트를 불러오는 구문
 // import Example_1 from './example/chapter_2';
-import Component from './example/chapter_3/MyComponent341';
+import Component from './example/chapter_3/MyComponent342';
 
 function App() {
   // return <Component.MyComponent name="React" />;
---
+++ src/example/chapter_3/MyComponent342.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+  // useState 사용 시, state의 초기값 형태를 자유롭게 넣으 수 있음
+  const [message, setMessage] = useState('');
+
+  const onClickEnter = () => setMessage('안녕하세요');
+  const onClickLeave = () => setMessage('안녕히 가세요');
+
+  return (
+    <div>
+      <button type="button" onClick={onClickEnter}>입장</button>
+      <button type="button" onClick={onClickLeave}>퇴장</button>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default Say;
