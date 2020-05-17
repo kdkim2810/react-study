26e1310fd15672a22cc32a4a4eddfcdae5edc324
kdkim2810 <kdkim2810@gmail.com>
2020-05-17 18:31:47 +0900

    4.3 useState 객체 사용

--- src/example/chapter_4/MyComponent43.js
+++ src/example/chapter_4/MyComponent43.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 
 const EventPracetice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
 
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onchangeMessage = (e) => setMessage(e.target.value);
+  const { username, message } = form;
+
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(`${username} : ${message}`);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    });
   };
+
   const onKeyPress = (e) => {
     if (e.key === 'Enter') {
       onClick();
@@ -25,14 +39,14 @@ const EventPracetice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력하세요"
         value={message}
-        onChange={onchangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button type="button" onClick={onClick}>확인</button>
