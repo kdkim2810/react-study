88d8e5636eb67a3e22e08460f2c064134f427627
kdkim2810 <kdkim2810@gmail.com>
2020-05-17 17:58:09 +0900

    4.1.1 이벤트를 사용할 때 주의사항 4.1.2 이벤트 종류

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'; // 리액트를 불러오는 구문
-// import Example_1 from './example/chapter_2';
-import Component from './example/chapter_3/MyComponent342';
+import Component from './example/chapter_4/MyComponent41';
 
 function App() {
-  // return <Component.MyComponent name="React" />;
   return <Component />;
 }
 
