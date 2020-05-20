0e7226351accf8f4a3b5929467cbd030d5d6b387
kdkim2810 <kdkim2810@gmail.com>
2020-05-20 18:49:20 +0900

    5.2.2 createRef를 통한 ref 설정

--- src/example/chapter_5/Ex511.js
+++ src/example/chapter_5/Ex511.js
@@ -8,6 +8,9 @@ class ValidationSample extends Component {
     validated: false,
   }
 
+  // ref 생성
+  inputRef = React.createRef();
+
   // 비밀번호 이벤트 핸들러
   handleChange = (e) => {
     this.setState({
@@ -16,16 +19,20 @@ class ValidationSample extends Component {
   }
 
   handleButtonClick = () => {
+    console.log(this.inputRef)
     this.setState({
       clicked: true,
       validated: this.state.password === '0000'
     })
+    // 버튼 클릭 시, input 박스에 포커스 지정
+    this.inputRef.current.focus();
   }
 
   render() {
     return (
       <div>
         <input type="password"
+        ref={this.inputRef}
         value={this.state.password} 
         onChange={this.handleChange}
         className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
