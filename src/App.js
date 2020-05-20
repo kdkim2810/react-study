9f88bfdde816f2c4e75786f747856e6c793df809
kdkim2810 <kdkim2810@gmail.com>
2020-05-20 19:06:21 +0900

    6.2 데이터 배열을 컴포넌트 배열로 전환하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react'; // 리액트를 불러오는 구문
-// import Component from './example/chapter_5/Ex532';
-import ScrollBox from './example/chapter_5/Ex532';
-
-class App extends Component {
-  // return <Component />;
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => {
-          this.ScrollBox = ref;
-        }}
-        />
-        <button
-          type="button"
-          onClick={() => this.ScrollBox.ScrollToBottom()}
-        >
-          맨 밑으로
-        </button>
-      </div>
-    );
-  }
+import React from 'react'; // 리액트를 불러오는 구문
+import Component from './example/chapter_6/Ex621';
+// import ScrollBox from './example/chapter_5/Ex532';
+function App() {
+  return <Component />;
 }
+// class App extends Component {
+//   // return <Component />;
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => {
+//           this.ScrollBox = ref;
+//         }}
+//         />
+//         <button
+//           type="button"
+//           onClick={() => this.ScrollBox.ScrollToBottom()}
+//         >
+//           맨 밑으로
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/example/chapter_6/Ex621.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const IterationSample = () => {
+  const names = ['눈사람', '얼음', '눈', '바람'];
+  const nameList = names.map((name) => <li>{name}</li>);
+
+  return <ul>{nameList}</ul>;
+};
+
+export default IterationSample;
