664d90113fac5d9349c484102a751c23a32700dd
kdkim2810 <kdkim2810@gmail.com>
2020-05-20 19:02:15 +0900

    5.3.3 Component에 ref 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'; // 리액트를 불러오는 구문
-import Component from './example/chapter_5/Ex511';
+import React, { Component } from 'react'; // 리액트를 불러오는 구문
+// import Component from './example/chapter_5/Ex532';
+import ScrollBox from './example/chapter_5/Ex532';
 
-function App() {
-  return <Component />;
+class App extends Component {
+  // return <Component />;
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => {
+          this.ScrollBox = ref;
+        }}
+        />
+        <button
+          type="button"
+          onClick={() => this.ScrollBox.ScrollToBottom()}
+        >
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
