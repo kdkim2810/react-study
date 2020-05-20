9819883c0e22486896d4b32dc6fcf12d59eb0746
kdkim2810 <kdkim2810@gmail.com>
2020-05-20 19:15:31 +0900

    6.4.2 데이터 추가 기능 구현하기

--- src/example/chapter_6/Ex621.js
+++ src/example/chapter_6/Ex621.js
@@ -8,9 +8,31 @@ const IterationSample = () => {
     { id: 4, text: '바람' },
   ]);
 
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+
+    setNextId(nextId + 1); // nextId값에 1을 추가
+    setNames(nextNames); // inputText값을 text값으로 사용
+    setInputText(''); // input필드를 초기화
+  };
+
   const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
 
-  return <ul>{nameList}</ul>;
+  return (
+    <div>
+      <input value={inputText} onChange={onChange} />
+      <button type="button" onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </div>
+  );
 };
 
 export default IterationSample;
